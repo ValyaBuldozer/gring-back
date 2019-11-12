9a54223a2dadbd6a9cff455f40c5a0b955fcfcea
Илья Остроух <ilya.ostrouh@yandex.ru>
2019-11-12 20:13:33 +0300

    some models changes #2

--- models/Timetable.py
+++ models/Timetable.py
@@ -10,6 +10,6 @@ class Timetable(db.Model):
         db.Integer, db.ForeignKey("public_place.object_id"), primary_key=True, name="public_place_id", nullable=False
     )
     public_place = relationship("PublicPlace")
-    week_day = db.Column(db.Enum(WeekDay), name="week_day", nullable=False)
+    week_day = db.Column(db.Enum(WeekDay), primary_key=True, name="week_day", nullable=False)
     open_time = db.Column(db.Time, name="open_time", nullable=False)
     close_time = db.Column(db.Time, name="close_time", nullable=False)
