92eaec9530632f1e3cf0b757cef399d6a908d431
ValyaBuldozeR <kiselek44@yandex.ru>
2019-11-12 18:55:49 +0300

    Fix table names at Geolocation and CategoryObject models.

--- models/CategoryObject.py
+++ models/CategoryObject.py
@@ -1,7 +1,7 @@
 from models.base import db
 
 
-CategoryObject = db.Table("cateogry_object", db.metadata,
+CategoryObject = db.Table("category_object", db.metadata,
     db.Column("object_id", db.Integer, db.ForeignKey("object.object_id"), nullable=False),
     db.Column("category_id", db.Integer, db.ForeignKey("category.category_id"), nullable=False)
 )
--- models/Geolocation.py
+++ models/Geolocation.py
@@ -6,4 +6,4 @@ class Geolocation(db.Model):
     __tablename__ = "geolocation"
     id = db.Column(db.Integer, primary_key=True, name="geolocation_id", nullable=False)
     latitude = db.Column(db.DECIMAL(10, 8), name="latitude", nullable=False)
-    longtude = db.Column(db.DECIMAL(11, 8), name="longtude", nullable=False)
+    longtude = db.Column(db.DECIMAL(11, 8), name="longitude", nullable=False)
