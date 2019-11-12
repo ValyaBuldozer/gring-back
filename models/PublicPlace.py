5f76060d918ac814191c7a1fd2e1d0518c2359a4
ValyaBuldozeR <kiselek44@yandex.ru>
2019-11-12 19:28:29 +0300

    Add timetable to PublicPlace model

--- models/PublicPlace.py
+++ models/PublicPlace.py
@@ -8,14 +8,24 @@ class PublicPlace(Object):
 
     __tablename__ = "public_place"
     id = db.Column(
-        db.Integer, db.ForeignKey("object.object_id"), primary_key=True, name="object_id", nullable=False
+        db.Integer,
+        db.ForeignKey("object.object_id"),
+        primary_key=True,
+        name="object_id",
+        nullable=False
     )
     name = db.Column(db.String(100), name="place_name", nullable=False)
-    address = db.Column(db.String(200), name="place_address", default="", nullable=False)
+    address = db.Column(
+        db.String(200), name="place_address", default="", nullable=False
+    )
     geolocation_id = db.Column(
-        db.Integer, db.ForeignKey("geolocation.geolocation_id"), name="place_geolocation_id", nullable=False
+        db.Integer,
+        db.ForeignKey("geolocation.geolocation_id"),
+        name="place_geolocation_id",
+        nullable=False
     )
     geolocation = relationship("Geolocation")
+    timetable = relationship("Timetable")
 
     __mapper_args__ = {
         'polymorphic_identity': ObjectType.public_place
--- models/Timetable.py
+++ models/Timetable.py
@@ -10,6 +10,6 @@ class Timetable(db.Model):
         db.Integer, db.ForeignKey("public_place.object_id"), primary_key=True, name="public_place_id", nullable=False
     )
     public_place = relationship("PublicPlace")
-    week_day = db.Column(db.Enum(WeekDay), name="week_day", nullable=False)
+    week_day = db.Column(db.Enum(WeekDay), name="week_day", nullable=False, primary_key=True)
     open_time = db.Column(db.Time, name="open_time", nullable=False)
     close_time = db.Column(db.Time, name="close_time", nullable=False)
