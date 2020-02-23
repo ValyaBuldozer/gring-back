37b418d908a376dc523a27da64f43172e582dccd
Илья Остроух <ilya.ostrouh@yandex.ru>
2020-02-23 17:53:30 +0300

    - updated adding user registration

--- routes/auth.py
+++ routes/auth.py
@@ -82,6 +82,18 @@ def update_user():
     content = g.data
     session = get_session()
 
+    users = session.query(User)
+
+    username = content['name']
+    if any(user.name == username for user in users):
+        abort(400, "User with name = %s already exist" % username)
+        return
+
+    usermail = content['email']
+    if any(user.email == usermail for user in users):
+        abort(400, "User with email = %s already exist" % usermail)
+        return
+
     roles = []
 
     for role_id in content['roles']:
