bd3c49e3febf7cc435494392d5f465afd3c38136
Илья Остроух <ilya.ostrouh@yandex.ru>
2020-06-19 04:14:05 +0300

    - unit tests update

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -18,7 +18,7 @@ def get_generated_text():
     return "text " + datetime.datetime.now().strftime("%d-%m-%Y %H:%M")
 
 
-class BasicTestCase(unittest.TestCase):
+class TestHelper(unittest.TestCase):
 
     username = ''
 
@@ -49,10 +49,24 @@ class BasicTestCase(unittest.TestCase):
     def logout(self):
         return self.app.delete('api/auth', follow_redirects=True)
 
+    def add_city(self, name, image_link, description):
+        return self.app.put(f'api/cities',
+                            data=json.dumps(dict(name=name, image_link=image_link, description=description)),
+                            query_string={'locale': 'en'}, content_type='application/json', follow_redirects=True)
+
+    def add_city_with_data(self):
+        title = get_generated_title()
+        text = get_generated_text()
+        return self.add_city(title, "image", text)
+
     def add_category(self, name, label):
         return self.app.put(f'api/categories',
                             data=json.dumps(dict(name=name, label=label)),
                             query_string={'locale': 'en'}, content_type='application/json', follow_redirects=True)
+
+    def add_category_with_data(self):
+        title = get_generated_title()
+        return self.add_category(title, title)
 
     def add_route(self, route_data):
         return self.app.put('api/routes',
@@ -63,6 +77,22 @@ class BasicTestCase(unittest.TestCase):
         return self.app.put('api/places',
                             data=place_data,
                             query_string={'locale': 'en'}, content_type='application/json', follow_redirects=True)
+
+    def add_place_with_data(self):
+        title = get_generated_title()
+        text = get_generated_text()
+        place_data = json.dumps(dict(
+            image_link="image",
+            audioguide_link="audio",
+            description=text,
+            city_id=1,
+            name=title,
+            address="address",
+            latitude=55,
+            longitude=55,
+            categories=[1]
+        ))
+        return self.add_place(place_data)
 
     def add_public_place(self, public_place_data):
         return self.app.put('api/public_places',
@@ -112,7 +142,7 @@ class BasicTestCase(unittest.TestCase):
         return self.app.delete(f'api/user/visited/{place_id}')
 
 
-class GetTestCase(BasicTestCase):
+class GetTestCase(TestHelper):
     def test_get_cities(self):
         response = self.app.get('/api/cities')
         self.assertEqual(response.status_code, 200)
@@ -134,7 +164,12 @@ class GetTestCase(BasicTestCase):
         self.assertEqual(response.status_code, 200)
 
 
-class AdminPutTestCase(BasicTestCase):
+class AdminPutTestCase(TestHelper):
+    def test_put_cities(self):
+        title = get_generated_title()
+        text = get_generated_text()
+        response = self.add_city(title, "image", text)
+        self.assertEqual(response.status_code, 200)
 
     def test_put_categories(self):
         title = get_generated_title()
@@ -142,7 +177,11 @@ class AdminPutTestCase(BasicTestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_put_route(self):
-        self.login('User1', '123')
+        response = self.login('User1', '123')
+        self.assertEqual(response.status_code, 200)
+        response = self.add_city_with_data()
+        self.assertEqual(response.status_code, 200)
+
         title = get_generated_title()
         text = get_generated_text()
         route_data = json.dumps(dict(
@@ -151,7 +190,7 @@ class AdminPutTestCase(BasicTestCase):
             places=[
                 {
                     "id": 1,
-                    "description": "description",
+                    "description": "in route " + text,
                     "audioguide": "audioguide"
                 }
             ],
@@ -161,7 +200,13 @@ class AdminPutTestCase(BasicTestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_put_place(self):
-        self.login('User1', '123')
+        response = self.login('User1', '123')
+        self.assertEqual(response.status_code, 200)
+        response = self.add_city_with_data()
+        self.assertEqual(response.status_code, 200)
+        response = self.add_category_with_data()
+        self.assertEqual(response.status_code, 200)
+
         title = get_generated_title()
         text = get_generated_text()
         place_data = json.dumps(dict(
@@ -179,7 +224,13 @@ class AdminPutTestCase(BasicTestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_put_public_place(self):
-        self.login('User1', '123')
+        response = self.login('User1', '123')
+        self.assertEqual(response.status_code, 200)
+        response = self.add_city_with_data()
+        self.assertEqual(response.status_code, 200)
+        response = self.add_category_with_data()
+        self.assertEqual(response.status_code, 200)
+
         title = get_generated_title()
         text = get_generated_text()
         public_place_data = json.dumps(dict(
@@ -205,13 +256,22 @@ class AdminPutTestCase(BasicTestCase):
                 },
             ],
             phone_number="88005553535",
-            avg_bill="1000 рублей"
+            avg_bill="1000 rubles"
         ))
         response = self.add_public_place(public_place_data)
         self.assertEqual(response.status_code, 200)
 
     def test_put_history_person(self):
-        self.login('User1', '123')
+        response = self.login('User1', '123')
+        self.assertEqual(response.status_code, 200)
+        response = self.add_city_with_data()
+        self.assertEqual(response.status_code, 200)
+        response = self.add_category_with_data()
+        self.assertEqual(response.status_code, 200)
+        for i in range(2):
+            response = self.add_place_with_data()
+            self.assertEqual(response.status_code, 200)
+
         title = get_generated_title()
         text = get_generated_text()
         person_data = json.dumps(dict(
@@ -229,24 +289,30 @@ class AdminPutTestCase(BasicTestCase):
         self.assertEqual(response.status_code, 200)
 
 
-class UserTestCase(AdminPutTestCase):
+class UserTestCase(TestHelper):
+
     def test_basic_register_new_user(self):
         self.generate_username()
         response = self.register(self.__class__.username, self.__class__.username + '@mail.ru', '123')
         self.assertEqual(response.status_code, 200)
 
     def test_login_logout(self):
-        rv = self.login(self.__class__.username, '123')
-        self.assertEqual(rv.status_code, 200)
-        rv = self.logout()
-        self.assertEqual(rv.status_code, 200)
-        rv = self.login('_' + self.__class__.username + '_', '123')
-        assert b'User not found' in rv.data
-        rv = self.login(self.__class__.username, '_123_')
-        assert b'Invalid password' in rv.data
+        response = self.login(self.__class__.username, '123')
+        self.assertEqual(response.status_code, 200)
+        response = self.logout()
+        self.assertEqual(response.status_code, 200)
+        response = self.login('_' + self.__class__.username + '_', '123')
+        assert b'User not found' in response.data
+        response = self.login(self.__class__.username, '_123_')
+        assert b'Invalid password' in response.data
 
     def test_reviews(self):
-        self.login('User1', '123')
+        response = self.login('User1', '123')
+        self.assertEqual(response.status_code, 200)
+        for i in range(2):
+            response = self.add_place_with_data()
+            self.assertEqual(response.status_code, 200)
+
         rating = randint(1, 5)
         self.login(self.__class__.username, '123')
         text = get_generated_text()
@@ -265,6 +331,11 @@ class UserTestCase(AdminPutTestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_favorites(self):
+        response = self.login('User1', '123')
+        self.assertEqual(response.status_code, 200)
+        response = self.add_place_with_data()
+        self.assertEqual(response.status_code, 200)
+
         self.login(self.__class__.username, '123')
         response = self.add_favorite(1)
         self.assertEqual(response.status_code, 200)
@@ -274,6 +345,11 @@ class UserTestCase(AdminPutTestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_visited_places(self):
+        response = self.login('User1', '123')
+        self.assertEqual(response.status_code, 200)
+        response = self.add_place_with_data()
+        self.assertEqual(response.status_code, 200)
+
         self.login(self.__class__.username, '123')
         response = self.add_visited(1)
         self.assertEqual(response.status_code, 200)
